03ef88e6e940b4ce0d824db04fff74c78ac9d23e
johnsondevin <91571848+johnsondevin@users.noreply.github.com>
2021-11-16 16:04:17 -0700

    Update point.py

---
+++ batter/game/point.py
@@ -0,0 +1,23 @@
+class Point:
+    def __init__(self, x, y):
+        self._x = x
+        self._y = y
+
+    def add(self, other):
+        x = self._x + other.get_x()
+        y = self._y + other.get_y()
+        return Point(x,y)
+
+    def equals(self, other):
+        return self._x == other.get_x() and self._y == other.get_y()
+
+    def get_x(self):
+        return self._x
+
+    def get_y(self):
+        return self._y
+    
+    def reverse(self):
+        x = self._x * -1
+        y = self._y * -1
+        return Point(x, y)
