e07983b99b4cc1972f0a5175c8883f6e16e7a374
johnsondevin <91571848+johnsondevin@users.noreply.github.com>
2021-11-18 15:58:09 -0700

    Update draw_actors_action.py

--- batter/game/draw_actors_action.py
+++ batter/game/draw_actors_action.py
@@ -3,11 +3,15 @@ from game.action import Action
 from output_service import OutputService
 
 class DrawActorsAction(Action):
+    
     def __init__(self, output_service):
         self._output = output_service
 
     def execute(self, cast):
+
         self._output.clear_screen()
+
         for i in cast:
             self._output.draw_actor(i)
+
         self._output.flush_buffer()
