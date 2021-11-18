9bc027496b92a27f35f22de5dd387a3f95f8f92e
johnsondevin <91571848+johnsondevin@users.noreply.github.com>
2021-11-18 15:57:43 -0700

    Update handle_collisions_action.py

--- batter/game/handle_collisions_action.py
+++ batter/game/handle_collisions_action.py
@@ -2,12 +2,15 @@ from game import constants
 from game.action import Action
 
 class HandleCollisionsAction(Action):
+    
     def execute(self, cast):
         paddle = cast['paddle']
         brick = cast['brick']
         ball = cast['ball'][0]
+
         if brick.get_position().equals(ball.get_position()):
             #Need function to delete brick
             pass
+
         if ball.get_position().equals(paddle.get_position() + 1):
             return ball.get_velocity() * -1
