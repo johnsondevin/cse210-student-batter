4443884b9d39aeb9d4893e78c8fb232704f7ae29
johnsondevin <91571848+johnsondevin@users.noreply.github.com>
2021-11-18 16:04:29 -0700

    Update move_actors_action.py

--- batter/game/move_actors_action.py
+++ batter/game/move_actors_action.py
@@ -1,3 +1,23 @@
-class MoveActorsAction:
-    def __init__(self):
-        pass
+from game import constants
+from game.action import Action
+from game.point import Point
+
+class MoveActorsAction(Action):
+    def execute(self, cast):
+        for group in cast.values():
+            for actor in group:
+                if not actor.get_velocity().is_zero():
+                    self._move_actor(actor)
+    
+    def _move_actor(self, actor):
+
+        position = actor.get_position()
+        velocity = actor.get_velocity()
+        x1 = position.get_x()
+        y1 = position.get_y()
+        x2 = velocity.get_x()
+        y2 = velocity.get_y()
+        x = 1 + (x1 + x2 - 1) % (constants.MAX_X - 1)
+        y = 1 + (y1 + y2 - 1) % (constants.MAX_Y - 1)
+        position = Point(x, y)
+        actor.set_position(position)
